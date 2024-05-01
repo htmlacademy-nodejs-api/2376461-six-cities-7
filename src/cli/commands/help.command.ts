49379c5ea190c8941ae4a4f2da5893163316f7a9
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-05-01 23:54:47 +0300

    Добавит комманды

---
+++ src/cli/commands/help.command.ts
@@ -0,0 +1,22 @@
+import chalk from 'chalk';
+
+import type { ICommand } from './command.interface.js';
+
+export class HelpCommand implements ICommand {
+  getName(): string {
+    return '--help';
+  }
+
+  async execute(..._parameters: string[]): Promise<void> {
+    console.info(`
+        ${chalk.yellowBright('Программа для подготовки данных для REST API сервера.')}
+        Пример:
+            ${chalk.blue('cli.js --<command> [--arguments]')}
+        Команды:
+            ${chalk.red('--help')}:                            # ${chalk.cyan('печатает этот текст')}
+            ${chalk.red('--version')}:                         # ${chalk.cyan('выводит номер версии')}
+            ${chalk.red('--import <path>')}:                   # ${chalk.cyan('импортирует данные из TSV')}
+            ${chalk.red('--generate <count> <path> <url>')}:   # ${chalk.cyan('гененирует данные в TSV')}
+    `);
+  }
+}
