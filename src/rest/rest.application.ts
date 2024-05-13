0936cc182d310e566154df7416d16dbcc0ac6382
IndieUnityMaker <ubivarik_serega@mail.ru>
2024-05-13 16:08:43 +0300

    установит пакет dotenv, добавит .env и реализует RestConfig для считывания данных из переменных окружения .env

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,12 +1,15 @@
 import { Logger } from '../shared/libs/logger/index.js';
+import { Config } from '../shared/libs/config/index.js';
 
 export class RestApplication {
   constructor (
-    private readonly logger: Logger
+    private readonly logger: Logger,
+    private readonly config: Config,
   ){}
 
   public async init(){
-    this.logger.info('APP init');
+    this.logger.info(`.env PORT: ${this.config.get('PORT')}`);
+    this.logger.info('rest.application was init');
   }
 
 }
